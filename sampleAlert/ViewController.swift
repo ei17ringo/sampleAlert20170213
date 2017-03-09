c94bd0bf87c4567e1663b61a51c5e168f2c67ba1
ei17ringo <ei17ringo@gmail.com>
2017-03-09 15:59:18 +0800

    commit init

--- sampleAlert/ViewController.swift
+++ sampleAlert/ViewController.swift
@@ -15,6 +15,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //　Checkボタンが押されたとき発動
+    @IBAction func tapCheck(_ sender: UIButton) {
+    
+        //部品のアラートを作る
+        let alertController = UIAlertController(title: "バッテリー残量", message: "後20%です", preferredStyle: .alert)
+        
+        //OKボタンを追加
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        
+        //アラートを表示する
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    // TODO:ボタンを新たにもう一つ作って、押したら「バッテリー残量100%です」と表示されるアラートを作成し、画面に表示するようにしてください。（100%の部分はmessageに指定しましょう）
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
